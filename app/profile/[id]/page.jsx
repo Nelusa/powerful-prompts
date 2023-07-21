ffee42fe5a746e4594876508712486d334f85ace
letochova <letochova@oksystem.cz>
2023-07-21 22:22:48 +0200

    Edit import

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams } from "next/navigation";
 
 import Profile from "@components/Profile";
 
